23fd62ac94bb71a00a113de0fdcf1175657b7e14
Chiradeep-Banik <chiradeep13@gmail.com>
2021-08-30 22:38:33 +0530

    started_some_backtracking_problems

---
+++ Random_problems/Rat_in_maze.js
@@ -0,0 +1,16 @@
+/**
+ * Backtracking --> Based on three things:
+ *      - Choice
+ *      - Constraints
+ *      - Goal
+ * 
+ */
+
+
+var maze = 
+[[1, 0, 0, 0],
+[1, 1, 0, 1], 
+[1, 1, 0, 0],
+[0, 1, 1, 1]];
+
+console.log(maze);
