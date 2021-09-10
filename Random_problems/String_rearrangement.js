c8ff3c29c4c38c74e2c966f1c5ceaeee98e61291
Chiradeep-Banik <chiradeep13@gmail.com>
2021-09-10 13:23:15 +0530

    cant_do

--- Random_problems/String_rearrangement.js
+++ Random_problems/String_rearrangement.js
@@ -1,8 +1,29 @@
 //Given an array of equal-length strings, you'd like to know if it's possible to rearrange the order of the elements in such a way that each consecutive pair of strings differ by exactly one character. Return true if it's possible, and false if not.
 
 function stringsRearrangement(inputArray) {
-    
-    return false;
+    for (let i = 0; i < inputArray.length; i++) {
+        var cur = inputArray[i];
+        var count_main = 0;
+        for (let j = 0; j < inputArray.length; j++) {
+            var temp = inputArray[j];
+            var count = 0;
+            for (let k = 0; k < cur.length; k++) {
+                if(cur[k] !== temp[k]){
+                    count++;
+                    console.log("count = ",count,"cur[k] = ",cur[k],"temp[k] = ",temp[k]);
+                }
+            }
+            if(count/2 === 1){
+                count_main++;
+            }
+            count = 0;
+        }
+        if(count_main/2 === 1){
+            return false;
+        }
+        count_main = 0;
+    }
+    return true;
 }
 
 var input = ["aba", "bbb", "bab"];
